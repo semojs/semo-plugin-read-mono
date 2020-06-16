3edd2298b91a256888ed5d483d0454e114925c78
Richard Yu <vipzhicheng@gmail.com>
2020-06-16 09:55:18 +0800

    fix: wechat format

--- packages/semo-plugin-read-extend-format-wechat/src/hooks/index.ts
+++ packages/semo-plugin-read-extend-format-wechat/src/hooks/index.ts
@@ -5,6 +5,8 @@ import wechatHandler from '../common/wechat'
  * 定义支持的格式
  */
 export const hook_read_define_format = {
-  wechat: '基于 Markdown 的微信公众号文章编辑器，基于国产的 Markdown Nice',
-  handler: wechatHandler
+  wechat: {
+    describe: '基于 Markdown 的微信公众号文章编辑器，基于国产的 Markdown Nice',
+    handler: wechatHandler
+  }
 }
